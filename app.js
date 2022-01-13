56dba40a8cd5a853688b86f0c09989a010f9870d
Aniks68 <ikechukwu.anene@decagon.dev>
2022-01-13 22:36:44 +0100

    third commit for initial setup for order & production functions

--- app.js
+++ app.js
@@ -15,11 +15,26 @@ console.log("Elvira House");
 
 function one() {
     console.log("Step 1 now a surbodinate");
-};
+}
 
 function two(call) {
     console.log("Step 2");
     call();
 }
 
-two(one);
+// two(one);
+let stocks = {
+    Fruits: ["strawberry", "grapes", "banana", "apple"]
+}
+console.log(stocks.Fruits[1]);
+
+let order = (prod) => {
+    console.log("Order placed, please alert production");
+    prod();
+};
+
+let production = () => {
+    console.log("Order received, beginnning production");
+};
+
+order(production);
