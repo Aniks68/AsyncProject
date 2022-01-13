1824e43da3f70f7178cda5de6c47fd33729d9ade
Aniks68 <ikechukwu.anene@decagon.dev>
2022-01-13 19:53:06 +0100

    second commit for callback refresher

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// synchronous
+/* // Synchronous
+console.log("I");
+console.log("eat ");
+console.log("ice-cream");
+console.log("with a ");
+console.log("spoon ");
+
+// Asynchronous
+console.log("I");
+console.log("am ");
+setTimeout(()=>{console.log("the head");}, 2500)
+console.log("of the");
+console.log("Elvira House");
+*/
+
+function one() {
+    console.log("Step 1 now a surbodinate");
+};
+
+function two(call) {
+    console.log("Step 2");
+    call();
+}
+
+two(one);
